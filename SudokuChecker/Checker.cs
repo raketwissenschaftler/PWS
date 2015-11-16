86c03a098127ac81d8bbf23d50026adbc219ad52
raketwissenschaftler <ricktrein@hotmail.com>
2015-11-16 21:59:01 +0100

    Getting rows works.

--- SudokuChecker/Checker.cs
+++ SudokuChecker/Checker.cs
@@ -11,6 +11,7 @@ namespace SudokuChecker
         private static string exampleSudoku = "000836009005090000600170040003600002070000918000050000050019400000460000009000037";
         private int[] sudokuArray;
         private int[][] squaresArray, horizontalColumns, verticalColumns;
+
         public Checker(string sudoku)
         {
             if (sudoku == "test")
@@ -18,6 +19,21 @@ namespace SudokuChecker
                 sudoku = exampleSudoku;
             }
             sudokuArray = sudoku.ToCharArray().Select(i => Int32.Parse(i.ToString())).ToArray();
+            if (sudokuArray.Length != 81)
+            {
+                throw new Exception("Invalid sudoku string");
+            }
+            GetRows();
+        }
+
+        private int[][] GetRows()
+        {
+            int[][] rowsList = new int[9][];
+            for(int i=0; i < 9; i++)
+            {
+                rowsList[i] = new List<int>(sudokuArray).GetRange(i * 9, 9).ToArray();
+            }
+            return rowsList;
         }
     }
 }
