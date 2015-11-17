0de7da91443d46ea86f049d26583a02e0f0e80f3
raketwissenschaftler <ricktrein@hotmail.com>
2015-11-17 15:06:02 +0100

    Created getcolumns and getsquares

--- SudokuChecker/Checker.cs
+++ SudokuChecker/Checker.cs
@@ -23,7 +23,6 @@ namespace SudokuChecker
             {
                 throw new Exception("Invalid sudoku string");
             }
-            GetRows();
         }
 
         private int[][] GetRows()
@@ -34,6 +33,41 @@ namespace SudokuChecker
                 rowsList[i] = new List<int>(sudokuArray).GetRange(i * 9, 9).ToArray();
             }
             return rowsList;
+        }
+        private int[][] GetColumns()
+        {
+            int[][] columnsList = new int[9][];
+            for (int i = 0; i < 9; i++)
+            {
+                int[] column = new int[9];
+                for (int c = 0; c < 9; c++)
+                {
+                    column[c] = sudokuArray[i + (c * 9)];
+                }
+                columnsList[i] = column;
+            }
+            return columnsList;
+        }
+
+        private int[][] GetSquares()
+        {
+            int[][] squares = new int[9][];
+            int[] startIndices = new int[9] { 0, 3, 6, 27, 30, 33, 54, 47, 60 };
+            int counter = 0;
+            foreach (int startIndex in startIndices)
+            {
+                List<int> squareList = new List<int>();
+                for (int i = startIndex; i < startIndex + 27; i = i + 9)
+                {
+                    for (int c = 0; c < 3; c++)
+                    {
+                        squareList.Add(sudokuArray[i + c]);
+                    }
+                }
+                squares[counter] = squareList.ToArray();
+                counter++;
+            }
+            return squares;
         }
     }
 }
